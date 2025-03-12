1f957346cde2aae8a83017c4549cd048c66997b5
gg-blake <blake092202@gmail.com>
2025-03-12 01:45:32 -0400

    attempt to fix fetch to github

---
+++ components/fetchGithubData.ts
@@ -0,0 +1,54 @@
+//@ts-nocheck
+const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    next: {revalidate: 3600}
+  };
+
+export default async function fetchGithubData() {
+    try {
+        const starredRepos = await fetch("https://api.github.com/users/gg-blake/starred", {next: {revalidate: 3600}})
+        .then((response) => response.json())
+        .then((values) => values.filter((val) => val.owner.login === 'gg-blake'))
+        .catch((error) => console.error(error));
+
+        const contentUrls = await Promise.all(starredRepos.map((val) => {
+            return fetch(val.contents_url.replace("{+path}", "README.md"), {next: {revalidate: 3600}})
+            .then(async (data) => {
+                const d = await data.json()
+                return {
+                    ...val,
+                    download_url: d.download_url
+                }
+            });
+        }))
+
+        const readmeData = await Promise.all(contentUrls.map((val) => {
+            return fetch(val.download_url, {next: {revalidate: 3600}})
+            .then(async (data) => {
+                const d = await data.text();
+
+                return {
+                    ...val,
+                    description_md: d
+                }
+            })
+            .catch(() => {
+                return {
+                    ...val,
+                    description_md: ""
+                }
+            })
+        }))
+        return readmeData
+    } catch (e) {
+        return [] as any[]
+    }
+    
+} 
+
+/*(async () => {
+    // Code that uses await
+    const result = await fetchGithubData()
+    console.log(result);
+})();*/
